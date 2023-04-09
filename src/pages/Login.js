c3e151f686a531e296c49443abff3717d2ff2507
Jubayer-cmd <sheikhabujubayer@gmail.com>
2023-04-09 15:42:42 +0600

    first commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,46 +1,69 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { LoginUser, googleLogin } from "../app/feature/auth/authSlice";
 import loginImage from "../assets/login.svg";
 const Login = () => {
   const { register, handleSubmit, reset } = useForm();
+  const { email, isLoading, isError, error } = useSelector(
+    (state) => state.auth
+  );
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const onSubmit = (data) => {
     console.log(data);
+    dispatch(LoginUser({ email: data.email, password: data.password }));
   };
 
+  useEffect(() => {
+    if (!isLoading && email) {
+      navigate("/");
+    }
+  }, [email, isLoading]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error);
+    }
+  }, [isError, error]);
+
+  const handleGoogleLogin = () => {
+    dispatch(googleLogin());
+  };
   return (
-    <div className='flex h-screen items-center'>
-      <div className='w-1/2'>
-        <img src={loginImage} className='h-full w-full' alt='' />
+    <div className="flex h-screen items-center">
+      <div className="w-1/2">
+        <img src={loginImage} className="h-full w-full" alt="" />
       </div>
-      <div className='w-1/2 grid place-items-center'>
-        <div className='bg-[#FFFAF4] rounded-lg grid place-items-center p-10'>
-          <h1 className='mb-10 font-medium text-2xl'>Login</h1>
+      <div className="w-1/2 grid place-items-center">
+        <div className="bg-[#FFFAF4] rounded-lg grid place-items-center p-10">
+          <h1 className="mb-10 font-medium text-2xl">Login</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className='space-y-3'>
-              <div className='flex flex-col items-start'>
-                <label htmlFor='email' className='ml-5'>
+            <div className="space-y-3">
+              <div className="flex flex-col items-start">
+                <label htmlFor="email" className="ml-5">
                   Email
                 </label>
-                <input type='email' {...register("email")} id='email' />
+                <input type="email" {...register("email")} id="email" />
               </div>
-              <div className='flex flex-col items-start'>
-                <label htmlFor='password' className='ml-5'>
+              <div className="flex flex-col items-start">
+                <label htmlFor="password" className="ml-5">
                   Password
                 </label>
                 <input
-                  type='password'
-                  id='password'
+                  type="password"
+                  id="password"
                   {...register("password")}
                 />
               </div>
-              <div className='relative !mt-8'>
+              <div className="relative !mt-8">
                 <button
-                  type='submit'
-                  className='font-bold text-white py-3 rounded-full bg-primary w-full'
+                  type="submit"
+                  className="font-bold text-white py-3 rounded-full bg-primary w-full"
                 >
                   Login
                 </button>
@@ -49,12 +72,19 @@ const Login = () => {
                 <p>
                   Don't have an account?{" "}
                   <span
-                    className='text-primary hover:underline cursor-pointer'
+                    className="text-primary hover:underline cursor-pointer"
                     onClick={() => navigate("/signup")}
                   >
                     Sign up
                   </span>
                 </p>
+                <button
+                  onClick={handleGoogleLogin}
+                  type="submit"
+                  className="font-bold text-white py-3 rounded-full bg-primary w-full"
+                >
+                  Login with Google
+                </button>
               </div>
             </div>
           </form>
